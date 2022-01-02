62a6038be290ba0c29ead823a60f88bb75db77ab
Andrea Vaccari <77388675+andre6226@users.noreply.github.com>
2022-01-02 15:08:56 +0100

    Add files via upload

---
+++ RSAlib/Chiave.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Cryptography;
+using System.IO;
+
+namespace RSAlib
+{
+    // Classi Chiave
+    public class Chiave
+    {
+        public RSAParameters Key { get; set; }
+
+        //Converte Key in una stringa in formato XML
+        public string keytostring()
+        {
+            var stringWriter = new StringWriter();
+            var xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+            xmlSerializer.Serialize(stringWriter, Key);
+            return stringWriter.ToString();
+        }
+
+    }
+
+    public class Chiavepub : Chiave
+    {
+        //Creo una nuova chiave pubblica assegnando a Key il valore della chiave pubblica creata dal Csp
+        public Chiavepub(int nbit, RSACryptoServiceProvider Csp)
+        {
+            Key = Csp.ExportParameters(false);
+        }
+        //Creo una nuova chiave pubblica assegnando a Key il valore di una chiave preesistente in formato stringa (XML)
+        public Chiavepub(string keystring)
+        {
+            var sr = new StringReader(keystring);
+            var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+            Key = (RSAParameters)xs.Deserialize(sr);
+        }
+
+    }
+
+
+    public class Chiavepriv : Chiave
+    {
+        //Creo una nuova chiave privata assegnando a Key il valore della chiave privata creata dal Csp
+        public Chiavepriv(int nbit, RSACryptoServiceProvider Csp)
+        {
+            Key = Csp.ExportParameters(true);
+        }
+        //Creo una nuova chiave privata assegnando a Key il valore di una chiave preesistente in formato stringa (XML)
+        public Chiavepriv(string keystring)
+        {
+            var sr = new StringReader(keystring);
+            var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+            Key = (RSAParameters)xs.Deserialize(sr);
+        }
+
+    }
+}
